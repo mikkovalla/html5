da103418298b6d5399bf7df1ab6e88d78ce75499
Mikko <mikko.vallaskivi@digia.com>
2021-05-18 00:29:30 +0300

    added fetch of 20 random fake users

--- participants/src/App.js
+++ participants/src/App.js
@@ -1,7 +1,23 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+  
+  const [people, peopleData] = useState({ participants: [] })
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'https://randomuser.me/api/?results=20'
+      );
+        peopleData(result.results)        
+    }
+    fetchData()
+  }, [])
+
+
   return (
     <div className="App">
       <Header />
