80b4cf8a82e0eae7dbfe60fbf56127ce31aecc71
Mikko <mikko.vallaskivi@digia.com>
2021-05-17 23:39:01 +0300

    added logo to header

--- participants/src/components/Header.js
+++ participants/src/components/Header.js
@@ -3,7 +3,7 @@ import './Header.css';
 const Header = () => {
     return (
         <div className='header'>
-        
+            <img src='logo_nord_soft.png' alt='logo' />
         </div>
     )
 }
