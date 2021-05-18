466cb43b2a53ceda35a44c0ea30ba87762b05cd3
Mikko <mikko.vallaskivi@digia.com>
2021-05-18 23:21:57 +0300

    added edit button to participant

--- participants/src/components/Participant.js
+++ participants/src/components/Participant.js
@@ -1,3 +1,4 @@
+import { FaPen, FaTrash } from "react-icons/fa"
 import './Participant.css'
 
 const Participant = (props) => {
@@ -6,6 +7,7 @@ const Participant = (props) => {
             <td>{props.person.name.first} {props.person.name.last}</td>
             <td>{props.person.email}</td>
             <td>{props.person.phone}</td>
+            <td className="button"><FaPen style={{ fill: '#909090'}} /></td>
         </tr>
     )
 }
